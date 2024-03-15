4201df178f263cf24236c22a3e21a773cd45f1b7
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-03-15 19:43:58 +0530

    add file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function formatDate(dateString) {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const year = String(date.getFullYear()).slice(2);
-    return ` ${day}/${month}/${year}`;
+    return ` ${day}-${month}-${year}`;
 }
 
 function fetchGitHubUser() {
